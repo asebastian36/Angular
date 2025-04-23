b53677af9732db342635cbf5ba96ac2c927f096e
asebastian36 <asebastianjob@gmail.com>
2025-04-23 15:04:30 -0600

    Avanzando en el curso de angular falta mas de la mitad de la seccion, a darle!!

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,8 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Gohan', power: 10000 },
+    { id: 2, name: 'Vegeta', power: 9000 },
+    { id: 3, name: 'Gohan', power: 4500 },
+    { id: 4, name: 'Trunks', power: 4500 },
+    { id: 5, name: 'Krilin', power: 1500 }
+  ]);
+
+  powerClasses = computed( () => {
+    return {
+      'text-danger': true
+    }
+  });
 
 }
